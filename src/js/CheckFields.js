347b163e34b97cfc698dc6b6d9c311520a6e2845
lauradsc <lauraasouzza05u@gmail.com>
2023-10-12 14:06:28 -0300

    commit check

--- src/js/CheckFields.js
+++
@@ -1,6 +0,0 @@
-export default class CheckFields {
-    static check(user, pwdValue) {
-        user && pwdValue != null ? window.location.href = "main.html": alert("preencha os campos");
-    }
-
-}
